6c90ca49d72bd1243eb260de3016f2101cc32897
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-15 19:38:28 +0300

    move firebase to firebase utility

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+//Web app's firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCfcvMAuhcYCAmoXyAf57GAcmBl6S7lvls",
   authDomain: "spotify-example-1e45a.firebaseapp.com",
@@ -11,6 +11,6 @@ const firebaseConfig = {
   appId: "1:358314423142:web:e771ea65a6d82c7e544ea7"
 };
 
-// Initialize Firebase
+//Initialize firebase and auth
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
