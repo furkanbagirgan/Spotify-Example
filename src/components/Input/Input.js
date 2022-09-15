761c1e694d6ff6033f015da4d53588412037854d
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-15 19:35:32 +0300

    finish input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {View, TextInput} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from '@expo/vector-icons/Ionicons';
 
 import styles from './Input.style';
 
 function Input(props) {
   //Here the input component is displayed on the screen.
   return (
-    <View style={styles.container}>
-      <Icon name={props.iconName} size={20} color="#A9A9A9" />
-      <TextInput {...props} style={styles.input} />
+    <View style={styles[props.theme].container}>
+      <Icon name={props.iconName} size={20} color={props.theme==='light'?'#A9A9A9':'#FFF'} />
+      <TextInput {...props} placeholderTextColor={props.theme==='light'?'#A9A9A9':'#FFF'} style={styles[props.theme].input} />
     </View>
   );
 }
--- src/components/Input/Input.style.js
+++ src/components/Input/Input.style.js
@@ -1,25 +1,49 @@
 import {StyleSheet} from 'react-native';
 
-//Here the styles of the input are created.
-const styles = StyleSheet.create({
+//Here the basic styles of the input are created.
+const basicStyles=StyleSheet.create({
   container: {
-    width: '100%',
+    width: '90%',
     height: 45,
     paddingHorizontal: 15,
     marginVertical: 10,
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    borderRadius: 15,
+    borderRadius: 22,
     borderWidth: 1,
-    borderColor: '#A9A9A9',
     backgroundColor: 'transparent',
   },
   input: {
     marginLeft: 5,
     flex: 1,
-    color: '#A9A9A9',
   },
 });
+
+//Here the changing styles of the input are created.
+const styles = {
+  light:StyleSheet.create({
+    ...basicStyles,
+    container: {
+      ...basicStyles.container,
+      borderColor: "#A9A9A9",
+    },
+    input:{
+      ...basicStyles.input,
+      color: "#A9A9A9"
+    }
+  }),
+  dark:StyleSheet.create({
+    ...basicStyles,
+    container: {
+      ...basicStyles.container,
+      borderColor: "#FFF",
+    },
+    input:{
+      ...basicStyles.input,
+      color: "#FFF"
+    }
+  }),
+};
 
 export default styles;
