8cfd234ee1c09eb10c192502c2afcab862a0f9bd
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-17 09:49:40 +0300

    finish detail screen

--- src/screens/Detail/Detail.js
+++ src/screens/Detail/Detail.js
@@ -1,109 +1,74 @@
-import React, {useEffect} from 'react';
-import {SafeAreaView, View, Text, Image, ActivityIndicator} from 'react-native';
+import React, {useEffect,useCallback} from 'react';
+import {SafeAreaView, FlatList, ActivityIndicator} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 
-import {getMovieDetail} from '../../redux/detailSlice';
+import {getListDetail} from '../../redux/detailSlice';
+import MusicCard from "../../components/MusicCard";
 import styles from './Detail.style';
 
 const Detail = ({route}) => {
   //Necessary states are created.
-  const {movieId} = route.params;
+  const {listId} = route.params;
   const theme = useSelector(state => state.theme.theme);
-  const movie = useSelector(state => state.detail.movie);
+  const likedMusics = useSelector(state => state.user.likedMusics);
+  const listMusics = useSelector(state => state.detail.listMusics);
   const loading = useSelector(state => state.detail.loading);
   const error = useSelector(state => state.detail.error);
   const dispatch = useDispatch();
 
   //When the page is opened for the first time, the movie detail is displayed.
   useEffect(() => {
-    dispatch(getMovieDetail(movieId));
-  }, [dispatch, movieId]);
+    dispatch(getListDetail(listId));
+  }, [dispatch, listId]);
 
-  //If loading is true this place works
-  if (loading || typeof movie !== 'object') {
+  //Here is the function where key assignments of the fields to repeat in the flat list are made.
+  const keyExtractor = (item) => {
+    return item.id;
+  };
+
+  //Here, there is a function that adjusts how the areas to be repeated in the
+  //flat list of newMusics will appear on the screen. Also, a MusicCard component is created for each chat.
+  const renderMusic = useCallback(({ item }) => {
+    const result=likedMusics.findIndex((music)=> {
+      return item.id===music.id;
+    });
+    const isLiked=result !== -1 ? true : false;
     return (
-      <SafeAreaView
-        style={
-          theme === 'light'
-            ? styles.lightLoadingContainer
-            : styles.darkLoadingContainer
-        }>
-        <ActivityIndicator size={40} color="C996CC" />
-      </SafeAreaView>
+      <MusicCard
+        music={item}
+        theme={theme}
+        isLiked={isLiked}
+      />
+    );
+  },[likedMusics]);
+
+  if (error) {
+    return (
+      <View style={styles[theme].errorWrapper}>
+        <Text style={styles[theme].errorText}>Connection Error</Text>
+      </View>
     );
   }
+
+  if (loading) {
+    return (
+      <View style={styles[theme].loadingContainer}>
+        <ActivityIndicator color="#A9A9A9" size={35} />
+      </View>
+    );
+  }
+
   //Here, the movie detail that will appear on the screen are created.
   return (
-    <SafeAreaView
-      style={theme === 'light' ? styles.lightContainer : styles.darkContainer}>
-      {error ? (
-        <View style={styles.errorWrapper}>
-          <Text style={styles.errorText}>Connection Error</Text>
-        </View>
-      ) : (
-        <>
-          <View style={styles.imageWrapper}>
-            <Image
-              source={{
-                uri: `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
-              }}
-              style={styles.image}
-            />
-          </View>
-          <View style={styles.detailWrapper}>
-            <Text
-              style={theme === 'light' ? styles.lightName : styles.darkName}>
-              {movie.title}
-            </Text>
-            <Text
-              style={
-                theme === 'light'
-                  ? styles.lightDescription
-                  : styles.darkDescription
-              }>
-              {movie.overview}
-            </Text>
-            <Text
-              style={theme === 'light' ? styles.lightVote : styles.darkVote}>
-              {Number(movie.vote_average).toFixed(1) + '/10'}
-            </Text>
-            <Text
-              style={
-                theme === 'light' ? styles.lightHeader : styles.darkHeader
-              }>
-              Genres:
-            </Text>
-            <View style={styles.genresWrapper}>
-              {movie.genres.map((item, index) => (
-                <Text
-                  key={item.id}
-                  style={
-                    theme === 'light' ? styles.lightGenre : styles.darkGenre
-                  }>
-                  {item.name + (movie.genres.length - 1 !== index ? ' | ' : '')}
-                </Text>
-              ))}
-            </View>
-            <Text
-              style={
-                theme === 'light' ? styles.lightHeader : styles.darkHeader
-              }>
-              Companies:
-            </Text>
-            <View style={styles.companyWrapper}>
-              {movie.production_companies.map((item, index) => (
-                <Text
-                  key={item.id}
-                  style={
-                    theme === 'light' ? styles.lightGenre : styles.darkGenre
-                  }>
-                  {item.name + (movie.genres.length - 1 !== index ? ' | ' : '')}
-                </Text>
-              ))}
-            </View>
-          </View>
-        </>
-      )}
+    <SafeAreaView style={styles[theme].container}>
+      <FlatList
+        contentContainerStyle={styles[theme].musicContainer}
+        keyExtractor={keyExtractor}
+        data={listMusics}
+        renderItem={renderMusic}
+        overScrollMode="never"
+        bounces={false}
+      />
     </SafeAreaView>
   );
 };
--- src/screens/Detail/Detail.style.js
+++ src/screens/Detail/Detail.style.js
@@ -1,112 +1,56 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet,StatusBar, Dimensions} from 'react-native';
 
-//Here the styles of the contactCard are created.
-const styles = StyleSheet.create({
-  lightContainer: {
+//Here the basic styles of the detail screen are created.
+const basicStyles=StyleSheet.create({
+  container: {
     flex: 1,
-    flexDirection: 'column',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    backgroundColor: 'white',
+    paddingHorizontal: 20
   },
-  darkContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    backgroundColor: '#1C0C5B',
-  },
-  lightLoadingContainer: {
+  errorWrapper: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
   },
-  darkLoadingContainer: {
+  errorText: {
+    color: 'red',
+    fontSize: 18,
+  },
+  musicContainer: {
+    width:'100%',
+  },
+  loadingContainer: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '1C0C5B',
-  },
-  imageWrapper: {
-    width: '100%',
-    height: 250,
-    alignSelf: 'flex-start',
-    borderRadius: 25,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 25,
-  },
-  detailWrapper: {
-    width: '100%',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-  },
-  lightName: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    color: 'black',
-    marginTop: 15,
-  },
-  darkName: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    color: '#C996CC',
-    marginTop: 15,
-  },
-  lightDescription: {
-    color: 'black',
-    marginVertical: 10,
-  },
-  darkDescription: {
-    color: '#C996CC',
-    marginVertical: 10,
-  },
-  lightVote: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 18,
-    marginVertical: 10,
-    alignSelf: 'center',
-  },
-  darkVote: {
-    color: '#C996CC',
-    fontWeight: 'bold',
-    fontSize: 18,
-    alignSelf: 'center',
-  },
-  genresWrapper: {
-    flexDirection: 'row',
-    marginBottom: 10,
-  },
-  lightGenre: {
-    color: 'black',
-    fontSize: 16,
-  },
-  darkGenre: {
-    color: '#C996CC',
-    fontSize: 16,
-  },
-  companyWrapper: {
-    flexDirection: 'row',
-  },
-  lightHeader: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 18,
-    marginVertical: 5,
-  },
-  darkHeader: {
-    color: '#C996CC',
-    fontWeight: 'bold',
-    fontSize: 18,
-    marginVertical: 5,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+//Here the changing styles of the detail screen are created.
+const styles = {
+  light: StyleSheet.create({
+    ...basicStyles,
+    container: {
+      ...basicStyles.container,
+      backgroundColor: 'white',
+    },
+    loadingContainer:{
+      ...basicStyles.loadingContainer,
+      backgroundColor: '#FFF'
+    }
+  }),
+  dark: StyleSheet.create({
+    ...basicStyles,
+    container: {
+      ...basicStyles.container,
+      backgroundColor: '#191414',
+    },
+    loadingContainer:{
+      ...basicStyles.loadingContainer,
+      backgroundColor: '#191414'
+    }
+  }),
+};
 
 export default styles;
